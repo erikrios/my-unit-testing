d72de261fcc947b944e7ebe1a942ee419d9f2845
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-21 12:18:05 +0700

    Create MainViewModelTest.kt

---
+++ app/src/test/java/io/erikrios/github/myunittesting/viewmodels/MainViewModelTest.kt
@@ -0,0 +1,24 @@
+package io.erikrios.github.myunittesting.viewmodels
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainViewModelTest {
+
+    @Test
+    fun getCircumreference() {
+    }
+
+    @Test
+    fun getSurfaceArea() {
+    }
+
+    @Test
+    fun getVolume() {
+    }
+
+    @Test
+    fun save() {
+    }
+}
