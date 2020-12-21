37ecc31f75010d99e33091f82e9ddefe5e7ab81c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-21 11:26:10 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/myunittesting/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myunittesting/MainActivity.kt
@@ -1,11 +1,22 @@
 package io.erikrios.github.myunittesting
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.myunittesting.databinding.ActivityMainBinding
+import io.erikrios.github.myunittesting.models.CuboidModel
+import io.erikrios.github.myunittesting.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        mainViewModel = MainViewModel(CuboidModel())
     }
 }
